c5a1f3a680550fdda78ab19c872523a48c00d27c
Julia Mikhailova <108729025+IuliiaMikhailova@users.noreply.github.com>
2022-08-11 12:29:17 +0300

    Add files via upload

---
+++ telegramBotHerokuColab-main/bot.py
@@ -0,0 +1,44 @@
+import telebot
+TOKEN = '5432712667:AAHmf46jUPR1pNMm8xtPXyNLzvz9L9TT34c'
+
+from telebot import types
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def welcome(message):
+	sti = open('welcome.png', 'rb')
+	bot.send_sticker(message.chat.id, sti)
+
+	#клавиатура
+	markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+	item1 = types.KeyboardButton("🧡 Репозиторий Лило")
+	item2 = types.KeyboardButton("😋 Напиши ей в личку")
+
+	markup.add(item1, item2)
+
+	bot.send_message(message.chat.id, "Привет тебе от Стича, {0.first_name}! Знакомься с моей Лило.".format(message.from_user, bot.get_me()),
+		parse_mode='html', reply_markup=markup)
+
+#назначаем действие для клавиатуры
+@bot.message_handler(content_types=['text'])
+def lalala(message):
+	if message.chat.type == 'private':
+		if message.text == '🧡 Репозиторий Лило':
+			bot.send_message(message.chat.id, 'https://github.com/IuliiaMikhailova')
+		elif message.text == '😋 Напиши ей в личку':
+			bot.send_message(message.chat.id, 'http://t.me/jullia_cherr')
+		else:
+			bot.send_message(message.chat.id, 'Не знаю что ответить😢')
+
+
+bot.polling(none_stop=True)
+
+
+
+
+
+
+
+
+#https://core.telegram.org/bots/api#available-methods
